97efab6f7ed1a0e1e9085779b6e14a979b5a58fe
Administrator <c.uvsuv@163.com>
2019-11-12 11:41:36 +0800

    修复问题

--- app/src/main/java/com/showmethe/speeddial/MainActivity.kt
+++ app/src/main/java/com/showmethe/speeddial/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.showmethe.speeddial
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.showmethe.speeddiallib.expand.Builder
 import com.showmethe.speeddiallib.expand.ExpandIcon
@@ -15,16 +16,20 @@ class MainActivity : AppCompatActivity() {
 
 
         val expands = ArrayList<ExpandIcon>()
-        expands.add(ExpandIcon().setIcon(R.mipmap.baseline_search_white_24).setBackgroundTint(R.color.colorPrimaryDark).setTextLabel("2222"))
-        expands.add(ExpandIcon().setIcon(R.mipmap.baseline_arrow_upward_white_24dp).setBackgroundTint(R.color.colorPrimaryDark).setTextLabel("333333"))
+        expands.add(ExpandIcon().setIcon(R.mipmap.baseline_search_white_24).setBackgroundTint(R.color.colorPrimaryDark).setTextLabel("项0"))
+        expands.add(ExpandIcon().setIcon(R.mipmap.baseline_arrow_upward_white_24dp).setBackgroundTint(R.color.colorPrimaryDark).setTextLabel("项1"))
         ExpandManager.newBuilder().setSlide(Builder.Slide.BOTTOM).setExpandIcons(expands).motion(R.color.black,R.mipmap.close).bindTarget(crl).build()
-
+        crl.setOnMenuClickListener {
+            Toast.makeText(this,"点击下方第${it}项目",Toast.LENGTH_SHORT).show()
+        }
 
 
         val expand2s = ArrayList<ExpandIcon>()
-        expand2s.add(ExpandIcon().setIcon(R.mipmap.baseline_search_white_24).setBackgroundTint(R.color.colorPrimaryDark).setTextLabel("2222"))
-        expand2s.add(ExpandIcon().setIcon(R.mipmap.baseline_arrow_upward_white_24dp).setBackgroundTint(R.color.colorPrimaryDark).setTextLabel("333333"))
+        expand2s.add(ExpandIcon().setIcon(R.mipmap.baseline_search_white_24).setBackgroundTint(R.color.colorPrimaryDark).setTextLabel("项0"))
+        expand2s.add(ExpandIcon().setIcon(R.mipmap.baseline_arrow_upward_white_24dp).setBackgroundTint(R.color.colorPrimaryDark).setTextLabel("项1"))
         ExpandManager.newBuilder().setSlide(Builder.Slide.TOP).setExpandIcons(expand2s).motion(R.color.black,R.mipmap.close).bindTarget(crl2).build()
-
+        crl2.setOnMenuClickListener {
+            Toast.makeText(this,"点击上方第${it}项目",Toast.LENGTH_SHORT).show()
+        }
     }
 }
